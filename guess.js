8065e227836e50b6da6b2d3437799cee909803a1
tessychen <66759126+tessychen@users.noreply.github.com>
2021-07-22 21:10:32 +0800

    Add files via upload

---
+++ guess.js
@@ -0,0 +1,13 @@
+﻿function getText() {
+    //12document.getElementById('btn').style.backgroundColor="white";
+
+    var input_str = document.getElementById('ans').value;//取輸入的值;
+    //如果沒有詩入任何東西，跳出alert。如果有東西才新增留言;
+    
+    if (input_str == "Kamon" || input_str == "kamon") {
+        window.location.href = 'katana.html';
+    }
+    else {
+        alert("you can only enter the right English word");
+    }
+}
